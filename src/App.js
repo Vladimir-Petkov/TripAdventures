2526d0278855e9fe002e2e23e328869f7f9b1f17
Vladimir-Petkov <57286575+Vladimir-Petkov@users.noreply.github.com>
2020-07-23 14:44:17 +0300

    Add navigation, some pictures and csss.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
+import Header from './Components/Header/Header';
+// import Footer from './Components/Footer/Footer';
 
 const App = () => {   
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* <Footer /> */}
     </div>
   );
 }
