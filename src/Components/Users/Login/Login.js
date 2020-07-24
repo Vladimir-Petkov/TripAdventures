91d345c255bd336a40124a04bc498140668824fa
Vladimir-Petkov <57286575+Vladimir-Petkov@users.noreply.github.com>
2020-07-24 17:48:08 +0300

    add

--- src/Components/Users/Login/Login.js
+++ src/Components/Users/Login/Login.js
@@ -1,35 +1,96 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Login = () => {
-    return (
-        <form action="/login" method="POST">
-            <div class="text-center mb-4">
-                <h1 class="h3 mb-3 font-weight-normal">Login</h1>
+class Login extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: '',
+            password: '',
+            usernameError: false,
+            passwordError: false,
+        };
+    };
+
+    onChange = (event, type) => {
+        const newState = {};
+        newState[type] = event.target.value;
+
+        this.setState(newState);
+    };
+
+    handleUsernameBlur = () => {
+        const { username } = this.state;
+
+        if (username.length < 4 || username.length > 8) {
+            this.setState({ usernameError: true });
+        } else {
+            this.setState({ usernameError: false });
+        }
+    };
+
+    handlePasswordBlur = () => {
+        const { password } = this.state;
+
+        if (password.length < 5 || password.length > 16) {
+            this.setState({ passwordError: true });
+        } else {
+            this.setState({ passwordError: false });
+        }
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { username, password, usernameError, passwordError } = this.state;
+
+        if (usernameError || passwordError) {
+            console.log('error')
+            return
+        }
+
+        console.log(username);
+        console.log(password);
+    };
+
+    render() {
+        const { username, password, usernameError, passwordError } = this.state;
+
+        const mystyle = {
+            textAlign: 'center',
+            color: "red"
+        };
+
+        return <form onSubmit={this.handleSubmit} method="POST">
+            <div className="text-center mb-4">
+                <h1 className="h3 mb-3 font-weight-normal">Login</h1>
             </div>
 
-            <div class="form-label-group">
-                <input type="text" id="inputUsername" name="username" class="form-control" placeholder="Username" required=""
-                    autofocus="" />
-                <label for="inputUsername">Username</label>
+            <div className="form-label-group">
+                {usernameError ? <div style={mystyle} >Username must be between 4 and 8 characters long</div> : null}
+                <label htmlFor="inputUsername">Username</label>
+                <input type="text" value={username} onChange={(e) => this.onChange(e, 'username')}
+                    onBlur={this.handleUsernameBlur} className="form-control" required />
             </div>
 
-            <div class="form-label-group">
-                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required="" />
-                <label for="inputPassword">Password</label>
+            <div className="form-label-group">
+                {passwordError ? <div style={mystyle} >Password must be between 5 and 16 characters long</div> : null}
+                <label htmlFor="inputPassword">Password</label>
+                <input type="password" value={password} onChange={(e) => this.onChange(e, 'password')} onBlur={this.handlePasswordBlur} className="form-control" required />
             </div>
 
-            <button class="btn btn-lg btn-dark btn-block" type="submit">Login</button>
+            <br />
+            <button className="btn btn-lg btn-dark btn-block" type="submit">Login</button>
 
-            <div class="text-center mb-4">
-                <p class="alreadyUser"> Don't have account? Then just register
-                    <Link to="/register"> Here</Link>!
-                </p>
+            <div className="text-center mb-4">
+                <p className="alreadyUser"> Don't have account? Then just register
+            <Link to="/register"> Here</Link>!
+        </p>
             </div>
 
-            <p class="mt-5 mb-3 text-muted text-center">© TripAdventures - 2019.</p>
+            <p className="mt-5 mb-3 text-muted text-center">© TripAdventures - 2019.</p>
         </form>
-    )
+    }
 };
 
 export default Login;
