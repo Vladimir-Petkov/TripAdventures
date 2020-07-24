dc3ac3d70097368ef679662203da0e4c6438b176
Vladimir-Petkov <57286575+Vladimir-Petkov@users.noreply.github.com>
2020-07-24 17:30:48 +0300

    Adding

--- src/Components/Users/Register/Register.js
+++ src/Components/Users/Register/Register.js
@@ -1,42 +1,115 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Register = () => {
-    return (
-        <form action="/register" method="POST">
-            <div class="text-center mb-4">
-                <h1 class="h3 mb-3 font-weight-normal">Register</h1>
+class Register extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: '',
+            password: '',
+            rePassword: '',
+            usernameError: false,
+            passwordError: false,
+            rePasswordError: false
+        };
+    };
+
+    onChange = (event, type) => {
+        const newState = {};
+        newState[type] = event.target.value;
+
+        this.setState(newState);
+    };
+
+    handleUsernameBlur = () => {
+        const { username } = this.state;
+
+        if (username.length < 4 || username.length > 8) {
+            this.setState({ usernameError: true });
+        } else {
+            this.setState({ usernameError: false });
+        }
+    };
+
+    handlePasswordBlur = () => {
+        const { password } = this.state;
+
+        if (password.length < 5 || password.length > 16) {
+            this.setState({ passwordError: true });
+        } else {
+            this.setState({ passwordError: false });
+        }
+    };
+
+    handleRepasswordBlur = () => {
+        const { password, rePassword } = this.state;
+
+        if (password !== rePassword) {
+            this.setState({ rePasswordError: true });
+        } else {
+            this.setState({ rePasswordError: false });
+        }
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { username, password, usernameError, passwordError, rePasswordError } = this.state;
+        
+        if (usernameError || passwordError || rePasswordError) {
+            console.log('error')
+            return
+        } 
+
+        console.log(username);
+        console.log(password);
+    };
+
+    render() {
+        const { username, password, rePassword, usernameError, passwordError, rePasswordError } = this.state;
+
+        const mystyle = {
+            textAlign: 'center',
+            color: "red"
+        };
+
+        return <form onSubmit={this.handleSubmit} method="POST">
+            <div className="text-center mb-4">
+                <h1 className="h3 mb-3 font-weight-normal">Register</h1>
                 <p>Join our family and make a wish!</p>
             </div>
 
-            <div class="form-label-group">
-                <input type="text" id="inputUsername" name="username" class="form-control" placeholder="Username" required=""
-                    autofocus="" />
-                <label for="inputUsername">Username</label>
+            <div className="form-label-group">
+                {usernameError ? <div style={mystyle} >Username must be between 4 and 8 characters long</div> : null}
+                <label htmlFor="inputUsername">Username</label>
+                <input type="text" value={username} onChange={(e) => this.onChange(e, 'username')}
+                    onBlur={this.handleUsernameBlur} className="form-control" required />
             </div>
 
-            <div class="form-label-group">
-                <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required="" />
-                <label for="inputPassword">Password</label>
+            <div className="form-label-group">
+            {passwordError ? <div style={mystyle} >Password must be between 5 and 16 characters long</div> : null}
+                <label htmlFor="inputPassword">Password</label>
+                <input type="password" value={password} onChange={(e) => this.onChange(e, 'password')} onBlur={this.handlePasswordBlur} className="form-control" required />
             </div>
 
-            <div class="form-label-group">
-                <input type="password" id="inputRePassword" name="rePassword" class="form-control" placeholder="Re-Password"
-                    required="" />
-                <label for="inputRePassword">Re-Password</label>
+            <div className="form-label-group">
+            {rePasswordError ? <div style={mystyle} >Repeat Password does not match password!</div> : null}
+                <label htmlFor="inputRePassword">Re-Password</label>
+                <input type="password" value={rePassword} onChange={(e) => this.onChange(e, 'rePassword')} onBlur={this.handleRepasswordBlur} className="form-control" required />
             </div>
 
-            <button class="btn btn-lg btn-dark btn-block" type="submit">Register</button>
+            <br />
+            <button className="btn btn-lg btn-dark btn-block" type="submit">Register</button>
 
-            <div class="text-center mb-4">
-                <p class="alreadyUser"> Already have account? Then just login
-                    <Link to="/login"> Here</Link>!
-                </p>
+            <div className="text-center mb-4">
+                <p className="alreadyUser"> Already have account? Then just login
+                <Link to="/login"> Here</Link>!
+            </p>
             </div>
 
-            <p class="mt-5 mb-3 text-muted text-center">© TripAdventures - 2020.</p>
+            <p className="mt-5 mb-3 text-muted text-center">© TripAdventures - 2020.</p>
         </form>
-    )
+    }
 };
 
 export default Register;
